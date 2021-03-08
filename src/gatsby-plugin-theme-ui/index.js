e3f1364c1b6947c71b3992fe3d78fdd0ce505420
Andrew <andrew.gaynor@ymail.com>
2021-03-08 12:48:58 -0500

    gatsby blog initial commit

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,18 @@
+/*
+ * Want to change your theme colors?
+ * Try uncommenting the color overrides below
+ * to go from default purple to a blue theme
+ */
+
+const darkBlue = `#007acc`
+const lightBlue = `#66E0FF`
+const blueGray = `#282c35`
+
+export default {
+    colors: {
+        text: blueGray,
+        primary: darkBlue,
+        heading: blueGray,
+        background: '#2f048a'
+    },
+}
