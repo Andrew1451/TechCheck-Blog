a97a8da043f7ed1488d4220bdf14b103ecb52871
Andrew <andrew.gaynor@ymail.com>
2021-03-14 13:47:15 -0400

    useState blog. style blog template

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -10,6 +10,13 @@ export default {
         h2: {
             fontFamily: 'Arial'
         },
+        p: {
+            fontSize: '19px',
+            lineHeight: '30px'
+        },
+        img: {
+            width: '100%'
+        },
     }
     
 }
